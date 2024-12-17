3fc79bf1d4b04c925acd30ac6ba44906decc6943
IckyTheBiggy <elcia116eb@gmail.com>
2024-12-17 20:30:53 +0000

    Texture helper class

--- Vertex.Engine/Rendering/Shaders/shader.vert
+++ Vertex.Engine/Rendering/Shaders/shader.vert
@@ -1,12 +1,12 @@
 #version 330 core
 
 layout(location = 0) in vec3 aPos;
-layout(location = 1) in vec3 aCol;
+layout(location = 1) in vec2 aTexCoord;
 
-out vec3 ourCol;
+out vec2 texCoord;
 
 void main(void)
 {
+    texCoord = aTexCoord;
     gl_Position = vec4(aPos, 1.0);
-    ourCol = aCol;
 }
--- Vertex.Engine/Rendering/Window.cs
+++ Vertex.Engine/Rendering/Window.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+
 using OpenTK.Graphics.OpenGL4;
 using OpenTK.Windowing.Common;
 using OpenTK.Windowing.Desktop;
@@ -9,12 +11,20 @@ namespace Vertex.Engine.Rendering
     {
         private readonly float[] _vertices =
         {
-            //Positions             //Colors
-            0.5f, -0.5f, 0.0f,      1.0f, 0.0f, 0.0f,
-            -0.5f, -0.5f, 0.0f,     0.0f, 1.0f, 0.0f,
-            0.0f, 0.5f, 0.0f,       0.0f, 0.0f, 1.0f
+            //Positions         //Texture Coords
+            0.5f, 0.5f, 0.0f,   1.0f, 1.0f,
+            0.5f, -0.5f, 0.0f,  1.0f, 0.0f,
+            -0.5f, -0.5f, 0.0f, 0.0f, 0.0f,
+            -0.5f, 0.5f, 0.0f,  0.0f, 1.0f
         };
 
+        private readonly uint[] _indices =
+        {
+            0, 1, 3,
+            1, 2, 3
+        };
+
+        private int _elementBufferObject;
         private int _vertexBufferObject;
         private int _vertexArrayObject;
 
@@ -32,20 +42,21 @@ namespace Vertex.Engine.Rendering
             GL.ClearColor(0.0f, 0.3f, 0.3f, 1.0f);
 
             _vertexBufferObject = GL.GenBuffer();
+
             GL.BindBuffer(BufferTarget.ArrayBuffer, _vertexBufferObject);
             GL.BufferData(BufferTarget.ArrayBuffer, _vertices.Length * sizeof(float), _vertices, BufferUsageHint.StaticDraw);
 
             _vertexArrayObject = GL.GenVertexArray();
             GL.BindVertexArray(_vertexArrayObject);
 
-            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 6 * sizeof(float), 0);
+            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
             GL.EnableVertexAttribArray(0);
-
-            GL.VertexAttribPointer(1, 3, VertexAttribPointerType.Float, false, 6 * sizeof(float), 3 * sizeof(float));
-            GL.EnableVertexAttribArray(1);
 
             _shader = new Shader("Rendering/Shaders/shader.vert", "Rendering/Shaders/shader.frag");
             _shader.Use();
+
+            _timer = new Stopwatch();
+            _timer.Start();
         }
 
         protected override void OnRenderFrame(FrameEventArgs args)
@@ -55,6 +66,13 @@ namespace Vertex.Engine.Rendering
             GL.Clear(ClearBufferMask.ColorBufferBit);
 
             _shader.Use();
+
+            double timerValue = _timer.Elapsed.TotalSeconds;
+            float greenValue = (float)Math.Sin(timerValue) / 2.0f + 0.5f;
+
+            int vertexColorLocation = GL.GetUniformLocation(_shader.Handle, "ourColor");
+
+            GL.Uniform4(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
 
             GL.BindVertexArray(_vertexArrayObject);
             GL.DrawArrays(PrimitiveType.Triangles, 0, 3);
