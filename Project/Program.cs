381d4ac12ae71699a92a0a6c1b50d2ee22ededcc
IckyTheBiggy <elcia116eb@gmail.com>
2024-12-21 21:05:31 +0000

    Raylib window setup

---
+++ Project/Program.cs
@@ -0,0 +1,13 @@
+﻿using Vertex.Engine.Rendering;
+
+namespace Program
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Window window = new Window(800, 600, "Vertex", 120);
+            window.Run();
+        }
+    }
+}
